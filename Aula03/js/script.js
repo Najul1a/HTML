09217554148104c6eda02e762538d0d8c626bb36
anna <anajugeremias@gmail.com>
2024-06-12 14:57:26 -0300

    Atividade Entrega

--- Aula03/js/script.js
+++ Aula03/js/script.js
@@ -13,9 +13,13 @@ const isLight = document.documentElement.classList.contains("light");
 
 const imagem = isLight ? "./img/imagem ana.jpeg" : "./img/azul.jpeg";
 
+const alertas = isLight ? "Uma jovem tirando um self depois que passou maquiagem" : "Uma menina no banheiro da escola tirando foto de blusa azul, e wide leg preta";
+
 document.querySelector("#perfil img").setAttribute("src", imagem);
+document.querySelector("#perfil img").setAttribute("alt", alertas);
 }
 
 //Modificar as cores/imagem de fundo e botão
 //Deixar com todos os links clicáveis
 //fazer com que o "ALT" se modificar a cada tema 
+
